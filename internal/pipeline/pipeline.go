3191590cd6781bf9afe7b0183ef0f914ef5d945a
Sebastian Seitz <bassistsagainstracists@gmx.net>
2026-07-26 12:52:25 +0200

    add pipe

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -1,12 +1,16 @@
 package pipeline
 
-type Step[T any] func(state T) (T, error)
+type Step[T any] func(state T) error
+
+func Pipe[T any](steps ...Step[T]) Step[T] {
+	return func(state T) error {
+		return Run(state, steps...)
+	}
+}
 
 func Run[T any](state T, steps ...Step[T]) error {
 	for _, s := range steps {
-		var err error
-		state, err = s(state)
-		if err != nil {
+		if err := s(state); err != nil {
 			return err
 		}
 	}
