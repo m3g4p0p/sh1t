40b8ad1973a4a8a705ef0adc0874b91558348634
Sebastian Seitz <bassistsagainstracists@gmx.net>
2026-07-26 15:56:59 +0200

    add parallel execution

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -1,8 +1,33 @@
 package pipeline
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 type Step[T any] func(ctx context.Context) (Step[T], error)
+
+type AnyStep = Step[any]
+
+func Parallel[T any](steps ...Step[T]) Step[T] {
+	return func(ctx context.Context) (Step[T], error) {
+		ctx, cancel := context.WithCancelCause(ctx)
+		defer cancel(nil)
+
+		var wg sync.WaitGroup
+
+		for _, step := range steps {
+			wg.Go(func() {
+				if err := Run(ctx, step); err != nil {
+					cancel(err)
+				}
+			})
+		}
+
+		wg.Wait()
+		return nil, context.Cause(ctx)
+	}
+}
 
 func Sequence[T any](steps ...Step[T]) Step[T] {
 	return func(ctx context.Context) (Step[T], error) {
